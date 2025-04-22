e407cf9efe2692117723214467bd8718cf22a0e0
LuisPiquinrey <piquin.rey@gmail.com>
2025-04-22 13:49:45 +0200

    Initial commit: Set up the foundational architecture for a RESTful API using Spring Boot and Maven. The project is configured to use Java 21 and includes the necessary dependencies for building a web service. Additionally, GitHub Actions has been integrated to enable continuous integration and deployment workflows.

---
+++ src/main/java/com/luispiquinrey/apiknot/ApiKnotApplication.java
@@ -0,0 +1,13 @@
+package com.luispiquinrey.apiknot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiKnotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiKnotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/luispiquinrey/apiknot/ApiKnotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.luispiquinrey.apiknot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiKnotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
