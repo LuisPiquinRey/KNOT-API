15740511c2dac17f10ab0b7dbd722f4fd1374467
LuisPiquinRey <piquin.rey@gmail.com>
2025-04-27 00:27:34 +0200

    In this project, Zipkin is integrated as a distributed tracing solution to monitor and trace requests across microservices. It is configured using Spring Boot, with Zipkin running as a Docker container on localhost:9411.

--- src/main/java/com/luispiquinrey/apiknot/ApiKnotApplication.java
+++ src/main/java/com/luispiquinrey/apiknot/ApiKnotApplication.java
@@ -1,15 +1,20 @@
 package com.luispiquinrey.apiknot;
 
+import io.sentry.Sentry;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
 @EnableCaching
-public class ApiKnotApplication {
+public class ApiKnotApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ApiKnotApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+	}
 }
