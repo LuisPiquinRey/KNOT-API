5899d257697018c27f97b30234faf95100c5e2e0
LuisPiquinRey <piquin.rey@gmail.com>
2025-06-11 18:05:29 +0200

    Add eureka Server

--- MicroservicesUsers/src/main/java/com/luispiquinrey/MicroservicesUsers/MicroservicesUsersApplication.java
+++ MicroservicesUsers/src/main/java/com/luispiquinrey/MicroservicesUsers/MicroservicesUsersApplication.java
@@ -2,8 +2,10 @@ package com.luispiquinrey.MicroservicesUsers;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 
 @SpringBootApplication
+@RefreshScope
 public class MicroservicesUsersApplication {
 
 	public static void main(String[] args) {
