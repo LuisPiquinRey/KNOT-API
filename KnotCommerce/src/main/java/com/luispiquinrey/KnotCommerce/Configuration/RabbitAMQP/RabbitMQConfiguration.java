f0a58c7516f91f545b091cdeafe4e484cab4d711
LuisPiquinRey <piquin.rey@gmail.com>
2025-06-17 09:15:23 +0200

    Service layer implementation

---
+++ KnotCommerce/src/main/java/com/luispiquinrey/KnotCommerce/Configuration/RabbitAMQP/RabbitMQConfiguration.java
@@ -0,0 +1,5 @@
+package com.luispiquinrey.KnotCommerce.Configuration.RabbitAMQP;
+
+public class RabbitMQConfiguration {
+    
+}
---
+++ KnotCommerce/src/main/java/com/luispiquinrey/KnotCommerce/Exceptions/CategoryCreationException.java
@@ -0,0 +1,23 @@
+package com.luispiquinrey.KnotCommerce.Exceptions;
+
+import java.time.Instant;
+import java.time.ZoneOffset;
+import java.time.format.DateTimeFormatter;
+
+public class CategoryCreationException extends RuntimeException{
+    private Integer id_Category;
+    public CategoryCreationException(String message,Integer id_Category){
+        super(message);
+        this.id_Category=id_Category;
+    }
+    @Override
+    public String getMessage() {
+        String emoji = "⚠️";
+        String timestamp = DateTimeFormatter.ISO_INSTANT.format(Instant.now().atOffset(ZoneOffset.UTC));
+        if (id_Category != null) {
+            return String.format("CategoryCreationException: %s [%s] %s (Product ID: %d)", emoji, timestamp, super.getMessage(), id_Category);
+        } else {
+            return String.format("CategoryCreationException: %s [%s] %s", emoji, timestamp, super.getMessage());
+        }
+    }
+}
