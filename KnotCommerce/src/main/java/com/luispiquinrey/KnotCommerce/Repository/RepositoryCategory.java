a025b1ef6f44eff1109daad7ae5af7c395a116a9
LuisPiquinRey <piquin.rey@gmail.com>
2025-06-17 08:49:15 +0200

    Add repositoryCategory

---
+++ KnotCommerce/src/main/java/com/luispiquinrey/KnotCommerce/Repository/RepositoryCategory.java
@@ -0,0 +1,9 @@
+package com.luispiquinrey.KnotCommerce.Repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.luispiquinrey.KnotCommerce.Entities.Category;
+
+public interface RepositoryCategory extends JpaRepository<Category, Integer>{
+
+}
