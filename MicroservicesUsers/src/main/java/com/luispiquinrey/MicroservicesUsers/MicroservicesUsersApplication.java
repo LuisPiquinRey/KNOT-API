afec36bb69a46c9aae24e090bcbc84b726e0aedd
LuisPiquinRey <piquin.rey@gmail.com>
2025-06-24 20:18:07 +0200

    Start to integrate shards to microservice user

--- MicroservicesUsers/src/main/java/com/luispiquinrey/MicroservicesUsers/MicroservicesUsersApplication.java
+++ MicroservicesUsers/src/main/java/com/luispiquinrey/MicroservicesUsers/MicroservicesUsersApplication.java
@@ -2,10 +2,12 @@ package com.luispiquinrey.MicroservicesUsers;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
 
 @SpringBootApplication
 @RefreshScope
+@EnableConfigurationProperties
 public class MicroservicesUsersApplication {
 
 	public static void main(String[] args) {
---
+++ MicroservicesUsers/src/main/resources/db/migration/V1__create_tables_primary.sql
@@ -0,0 +1,7 @@
+CREATE TABLE User_Primary (
+    id_user BIGINT AUTO_INCREMENT PRIMARY KEY,
+    username VARCHAR(255) NOT NULL UNIQUE,
+    email VARCHAR(255),
+    password VARCHAR(255) NOT NULL,
+    role VARCHAR(50) NOT NULL DEFAULT 'ROLE_USER'
+);
